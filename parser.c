560f10e048ece6814d3655a39c86348674b52504
karintomania <19652340+karintomania@users.noreply.github.com>
2026-02-20 23:29:41 +0000

    parser is working

--- parser.c
+++ parser.c
@@ -40,7 +40,7 @@ struct AST {
   };
 };
 
-void printAstRecursive(AST *ast, int depth) {
+void print_ast_recursive(AST *ast, int depth) {
   if (ast->type == AST_TYPE_BINARY_OPERATOR) {
     char *op_str;
     switch (ast->astBO.operator) {
@@ -57,19 +57,18 @@ void printAstRecursive(AST *ast, int depth) {
         op_str = "/";
         break;
     }
-    printf("%*snumber:%s\n", depth * 2, " ", op_str);
+    printf("%*sop:%s\n", depth * 2, " ", op_str);
 
-    printAstRecursive(ast->astBO.left, depth+1);
-    printAstRecursive(ast->astBO.right, depth+1);
+    print_ast_recursive(ast->astBO.left, depth+1);
+    print_ast_recursive(ast->astBO.right, depth+1);
   } else {
-    printf("%*snumber:%f\n", depth * 2, " ", ast->astNum.number);
+    printf("%*snum:%f\n", depth * 2, " ", ast->astNum.number);
   }
 }
 
-void printAst(AST *ast) {
-  printAstRecursive(ast, 0);
+void print_ast(AST *ast) {
+  print_ast_recursive(ast, 0);
 }
-
 
 /*---------------------
  Main Parse logic
@@ -77,40 +76,73 @@ void printAst(AST *ast) {
 
 int position;
 AST *current;
+int parser_token_count;
 
-AST *parse_number(Token *tokens);
 AST *parse_term2(Token *tokens);
+AST *parse_term1(Token *tokens);
+AST *parse_number(Token *tokens);
 
 AST *parse_term2(Token *tokens) {
+  current = parse_term1(tokens);
+
+  while (position < parser_token_count) {
+    AST *ast = malloc(sizeof(AST));
+
+    Token t = tokens[position];
+
+    if (t.type != TKN_PLUS && t.type != TKN_MINUS) break;
+
+    position++;
+    *ast = (AST){
+      .type = AST_TYPE_BINARY_OPERATOR,
+      .astBO = {
+        .operator = (t.type == TKN_PLUS) ? OP_PLUS : OP_MINUS,
+        .left = current,
+        .right = parse_term1(tokens),
+      },
+    };
+
+    current = ast;
+  }
+
+  return current;
+}
+
+AST *parse_term1(Token *tokens) {
   current = parse_number(tokens);
 
-  AST *ast = malloc(sizeof(AST));
+  while (position < parser_token_count) {
+      AST *ast = malloc(sizeof(AST));
 
-  Token t = tokens[position++];
-  *ast = (AST){
-    .type = AST_TYPE_BINARY_OPERATOR,
-    .astBO = {
-      .operator = (t.type == TKN_PLUS) ? OP_PLUS : OP_MINUS,
-      .left = current,
-      .right = parse_number(tokens),
-    },
-  };
+      Token t = tokens[position];
 
-  current = ast;
+    if (t.type != TKN_MULT && t.type != TKN_DIV) break;
 
-  return ast;
+    position++;
+    *ast = (AST){
+      .type = AST_TYPE_BINARY_OPERATOR,
+      .astBO = {
+        .operator = (t.type == TKN_MULT) ? OP_MULT : OP_DIV,
+        .left = current,
+        .right = parse_number(tokens),
+      },
+    };
+
+      current = ast;
+    }
+
+  return current;
 }
 
 AST *parse_number(Token *tokens) {
-  Token t = tokens[position];
-  position++;
+  Token t = tokens[position++];
   
   AST *ast = malloc(sizeof(AST));
 
   *ast = (AST){
     .type = AST_TYPE_NUMBER,
     .astNum = {
-      .number = 1.0,
+      .number = t.num,
     },
   };
 
@@ -118,18 +150,20 @@ AST *parse_number(Token *tokens) {
 }
 
 
-AST *run_parser(Token *tokens, int token_n) {
+AST *run_parser(Token *tokens, int token_count) {
+  parser_token_count = token_count;
   position = 0;
-  current = parse_number(tokens);
 
-  while (position < token_n) {
-    position++;
-  }
   parse_term2(tokens);
 
   return current;
 }
 
 // free all ASTs
-void deinitAst(AST *ast);
-
+void deinitAst(AST *ast) {
+  if (ast->type == AST_TYPE_BINARY_OPERATOR) {
+    deinitAst(ast->astBO.right);
+    deinitAst(ast->astBO.left);
+  }
+  free(ast);
+}
--- test.c
+++ test.c
@@ -10,18 +10,20 @@
 ------------------------*/
 int test_run_lexer(void) {
   Token tokens[MAX_TOKENS];
-  char *str = "1 - 2.1 * -3 / 100";
+  char *str = "0 + 1 - 2.1 * -3 / 100";
   int token_count = run_lexer(str, tokens);
 
-  assert(7 == token_count);
+  assert(9 == token_count);
 
-  assert(token_equals(&tokens[0], &(Token){.type = TKN_NUMBER, .num = 1}));
-  assert(token_equals(&tokens[1], &(Token){.type = TKN_MINUS}));
-  assert(token_equals(&tokens[2], &(Token){.type = TKN_NUMBER, .num = 2.1}));
-  assert(token_equals(&tokens[3], &(Token){.type = TKN_MULT}));
-  assert(token_equals(&tokens[4], &(Token){.type = TKN_NUMBER, .num = -3}));
-  assert(token_equals(&tokens[5], &(Token){.type = TKN_DIV}));
-  assert(token_equals(&tokens[6], &(Token){.type = TKN_NUMBER, .num = 100}));
+  assert(token_equals(&tokens[0], &(Token){.type = TKN_NUMBER, .num = 0}));
+  assert(token_equals(&tokens[1], &(Token){.type = TKN_PLUS}));
+  assert(token_equals(&tokens[2], &(Token){.type = TKN_NUMBER, .num = 1}));
+  assert(token_equals(&tokens[3], &(Token){.type = TKN_MINUS}));
+  assert(token_equals(&tokens[4], &(Token){.type = TKN_NUMBER, .num = 2.1}));
+  assert(token_equals(&tokens[5], &(Token){.type = TKN_MULT}));
+  assert(token_equals(&tokens[6], &(Token){.type = TKN_NUMBER, .num = -3}));
+  assert(token_equals(&tokens[7], &(Token){.type = TKN_DIV}));
+  assert(token_equals(&tokens[8], &(Token){.type = TKN_NUMBER, .num = 100}));
 
   // test minus signs
   str = "-1 - -2.1";
@@ -42,14 +44,38 @@ int test_run_lexer(void) {
 
 int test_run_parser(void) {
   Token tokens[MAX_TOKENS];
-  int result = run_lexer("1 + 2", tokens);
-  assert(result == 3);
+  int result = run_lexer("1 + 2 / 4 - 8", tokens);
 
-  AST *ast = run_parser(tokens);
+  AST *ast = run_parser(tokens, result);
 
-  assert(ast->type == AST_TYPE_NUMBER);
+  assert(ast->type == AST_TYPE_BINARY_OPERATOR);
+  assert(ast->astBO.operator == OP_MINUS);
+  AST *l = ast->astBO.left;
+  AST *r = ast->astBO.right;
 
-  printAst(ast);
+  assert(l->type == AST_TYPE_BINARY_OPERATOR);
+  assert(l->astBO.operator == OP_PLUS);
+  AST *l_l = l->astBO.left;
+  AST *l_r = l->astBO.right;
+
+  assert(l_l->type == AST_TYPE_NUMBER);
+  assert(l_l->astNum.number == 1);
+
+  assert(l_r->type == AST_TYPE_BINARY_OPERATOR);
+  assert(l_r->astBO.operator == OP_DIV);
+  AST *l_r_l = l_r->astBO.left;
+  AST *l_r_r = l_r->astBO.right;
+
+  assert(l_r_l->type == AST_TYPE_NUMBER);
+  assert(l_r_l->astNum.number == 2);
+
+  assert(l_r_r->type == AST_TYPE_NUMBER);
+  assert(l_r_r->astNum.number == 4);
+
+  assert(r->type == AST_TYPE_NUMBER);
+  assert(r->astNum.number == 8);
+
+  deinitAst(ast);
 
   return 1;
 }
